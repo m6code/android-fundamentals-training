6747ce2c55d4ea689ae4164f969491bdde09960b
M6code <benjamynfrank101@gmail.com>
2020-12-22 11:07:50 +0100

    Complete HelloToast

--- HelloToast/app/src/main/java/com/m6code/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/m6code/hellotoast/MainActivity.java
@@ -1,14 +1,37 @@
 package com.m6code.hellotoast;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.textView_show_count);
+
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(MainActivity.this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
