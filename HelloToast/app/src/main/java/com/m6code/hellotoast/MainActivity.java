dd238bd17ffd5ccb7ac43131fb3f62deff7b2676
M6code <benjamynfrank101@gmail.com>
2020-12-22 19:59:03 +0100

    Complete HelloToast Part B

--- HelloToast/app/src/main/java/com/m6code/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/m6code/hellotoast/MainActivity.java
@@ -1,6 +1,7 @@
 package com.m6code.hellotoast;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -8,6 +9,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    public final String TAG2 = MainActivity.this.getPackageName();
 
     private int mCount = 0;
     private TextView mShowCount;
@@ -26,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     public void showToast(View view) {
         Toast toast = Toast.makeText(MainActivity.this, R.string.toast_message, Toast.LENGTH_LONG);
         toast.show();
+        Log.d(TAG, "showToast: getSimpleName = " + MainActivity.class.getSimpleName());
+        Log.d(TAG2, "showToast: getPackageName = " + MainActivity.this.getPackageName());
     }
 
     public void countUp(View view) {
