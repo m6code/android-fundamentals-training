e5df0f5e9d3eff6e303683d3efca7d8b9f2885d6
M6code <benjamynfrank101@gmail.com>
2020-12-31 10:51:44 +0100

    Activities and intents : Task 1 - Create the TwoActivities project

--- TwoActivities/app/src/main/java/com/m6code/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/m6code/twoactivities/MainActivity.java
@@ -3,12 +3,20 @@ package com.m6code.twoactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(TAG, "launchSecondActivity: Button Clicked");
     }
 }
