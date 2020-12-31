59669e829884ae5ad5023620e109edc60d5dff8c
M6code <benjamynfrank101@gmail.com>
2020-12-31 12:28:24 +0100

    Activities and intents : Task 4 - Return data back to the main Activity

--- TwoActivities/app/src/main/java/com/m6code/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/m6code/twoactivities/MainActivity.java
@@ -1,24 +1,31 @@
 package com.m6code.twoactivities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE = "com.m6code.twoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+    private static final String TAG = MainActivity.class.getSimpleName();
     private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
@@ -26,6 +33,19 @@ public class MainActivity extends AppCompatActivity {
         String message = mMessageEditText.getText().toString();
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST){
+            if (resultCode == RESULT_OK){
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
--- TwoActivities/app/src/main/java/com/m6code/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/m6code/twoactivities/SecondActivity.java
@@ -2,22 +2,35 @@ package com.m6code.twoactivities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    public static final String EXTRA_REPLY = "com.m6code.twoactivities.extra.REPLY";
+    private EditText mReply;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+        mReply=findViewById(R.id.editText_second);
         Intent intent = getIntent();
 
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+    }
+
+    public void returnReplay(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
