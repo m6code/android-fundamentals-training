8a04529099ece3afeff189606d776eee0d7179e9
M6code <benjamynfrank101@gmail.com>
2021-01-09 19:12:14 +0100

    Implicit Intents : Task 2, 3 & 4 - Implement the Open Website, Location, & Text button

--- ImplicitIntents/app/src/main/java/com/m6code/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/com/m6code/implicitintents/MainActivity.java
@@ -1,14 +1,59 @@
 package com.m6code.implicitintents;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText mWebsiteEditText, mLocationEditText, mShareTextEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_editText);
+        mLocationEditText = findViewById(R.id.location_editText);
+        mShareTextEditText = findViewById(R.id.share_editText);
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(TAG, "openLocation: Can't handle this intent");
+        }
+    }
+
+    public void openText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.share_chooser_title)
+                .setText(txt)
+                .startChooser();
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(TAG, "openLocation: Can't handle this!");
+        }
     }
 }
