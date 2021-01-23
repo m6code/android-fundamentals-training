7d7289442e84c8a75174cb83255366684e067066
M6code <benjamynfrank101@gmail.com>
2021-01-23 10:29:21 +0100

    Input controls:  Task 3 - Use a spinner for user choices

--- DroidCafeInput/app/src/main/java/com/m6code/droidcafe/OrderActivity.java
+++ DroidCafeInput/app/src/main/java/com/m6code/droidcafe/OrderActivity.java
@@ -3,13 +3,16 @@ package com.m6code.droidcafe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,21 @@ public class OrderActivity extends AppCompatActivity {
                 intent.getStringExtra(MainActivity.EXTRA_MSG);
         TextView textView = findViewById(R.id.order_textView);
         textView.setText(message);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null){
+            spinner.setOnItemSelectedListener(this);
+        }
+        // Create ArrayAdapter using the string array and default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        if(spinner != null){
+            spinner.setAdapter(adapter);
+        }
     }
 
     public void onRadioButtonClicked(View view) {
@@ -46,5 +64,16 @@ public class OrderActivity extends AppCompatActivity {
 
     public void displayToast(String msg) {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String spinnerLabel = parent.getItemAtPosition(position).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
