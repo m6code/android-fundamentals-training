08e96fc0276f1a750244d457ed7592e81bb05288
M6code <benjamynfrank101@gmail.com>
2021-01-23 09:56:39 +0100

    Input controls: Task 2 - Use radio buttons

--- DroidCafeInput/app/src/main/java/com/m6code/droidcafe/OrderActivity.java
+++ DroidCafeInput/app/src/main/java/com/m6code/droidcafe/OrderActivity.java
@@ -1,10 +1,13 @@
 package com.m6code.droidcafe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -14,9 +17,34 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
 
         Intent intent = getIntent();
-        String message = "Order: "+
+        String message = "Order: " +
                 intent.getStringExtra(MainActivity.EXTRA_MSG);
         TextView textView = findViewById(R.id.order_textView);
         textView.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void displayToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
