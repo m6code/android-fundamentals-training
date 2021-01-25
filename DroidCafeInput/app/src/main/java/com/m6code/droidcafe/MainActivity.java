d3d116fbb46d80e485097aff486dd917c4f6a1a9
M6code <benjamynfrank101@gmail.com>
2021-01-25 10:22:48 +0100

    Menu and Pickers :  Task 3 - Handle the selected menu item

--- DroidCafeInput/app/src/main/java/com/m6code/droidcafe/MainActivity.java
+++ DroidCafeInput/app/src/main/java/com/m6code/droidcafe/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -44,11 +45,31 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_order) {
-            return true;
+        switch (id){
+            case R.id.action_order :
+                Intent intent =new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MSG, mOrderMessage);
+                startActivity(intent);
+//                displayToast(getString(R.string.action_order_message));
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
+                // Do nothing
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
