e700f2d11cb97940e12435eec2e91aebfa015636
M6code <benjamynfrank101@gmail.com>
2021-03-08 11:20:52 +0100

    AsyncTask : Homework completed

--- SimpleAsyncTask/app/src/main/java/com/m6code/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/com/m6code/simpleasynctask/MainActivity.java
@@ -5,33 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
     private static final String TEXT_STATE = "currentText";
+    private static final String PROGRESS_STATE ="currentProgress";
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.textView1);
+        mProgressBar = findViewById(R.id.progress_bar);
 
         // Restore TextView if there is a savedInstanceState
         if (savedInstanceState != null) {
             mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+            mProgressBar.setProgress(savedInstanceState.getInt(PROGRESS_STATE,0));
         }
     }
 
     public void startTask(View view) {
         mTextView.setText(R.string.napping);
-        new SimpleAsyncTask(mTextView).execute();
+        new SimpleAsyncTask(mTextView, mProgressBar).execute();
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(TEXT_STATE, mTextView.getText().toString());
+        outState.putInt(PROGRESS_STATE, mProgressBar.getProgress());
     }
 }
--- SimpleAsyncTask/app/src/main/java/com/m6code/simpleasynctask/SimpleAsyncTask.java
+++ SimpleAsyncTask/app/src/main/java/com/m6code/simpleasynctask/SimpleAsyncTask.java
@@ -1,6 +1,7 @@
 package com.m6code.simpleasynctask;
 
 import android.os.AsyncTask;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
@@ -8,22 +9,26 @@ import java.util.Random;
 
 public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
 
-    private WeakReference<TextView> mTextView;
+    private final WeakReference<TextView> mTextView;
+    private final WeakReference<ProgressBar> mProgress;
 
-    SimpleAsyncTask(TextView tv) {
+    SimpleAsyncTask(TextView tv, ProgressBar progressBar) {
         mTextView = new WeakReference<>(tv);
+        mProgress = new WeakReference<>(progressBar);
     }
 
     @Override
     protected String doInBackground(Void... voids) {
         Random random = new Random();
         int n = random.nextInt(11);
-        int s = n * 200;
+        int s = n * 20000;
 
         for (int i = 0; i < s; i++) {
             if (isCancelled()) break;
             else {
-                publishProgress(i);
+                // TODO: Convert value to percentage before publishing progress;
+                int per = (int) (i * 100 / s + 0.5);
+                publishProgress(per);
             }
         }
 
@@ -33,7 +38,7 @@ public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
             e.printStackTrace();
         }
 
-        return "Awake at last after sleeping for " + s + " milliseconds";
+        return String.valueOf(s);
 
     }
 
@@ -41,13 +46,17 @@ public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
     protected void onPostExecute(String s) {
         // Because mTextView is a weak reference, you have to deference it with the get()
         // method to get the underlying TextView object, and to call setText() on it.
-        mTextView.get().setText(s);
+        mTextView.get().setText("Awake at last after sleeping for " + s + " milliseconds");
+        mProgress.get().setProgress(Integer.parseInt(s));
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
 
-        mTextView.get().setText("Napping - Sleep Time Counting.. " + values[0]);
+//        if (mTextView.get() != null) {
+            mTextView.get().setText("Napping - Sleep Time Counting.. " + values[0] + "%");
+            mProgress.get().setProgress(values[0]);
+//        }
     }
 }
