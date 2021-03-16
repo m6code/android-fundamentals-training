a91cf69749d9779d9d4c2739386e23084804f9bd
M6code <benjamynfrank101@gmail.com>
2021-03-16 11:36:25 +0100

    Notifications : Task 2 - Update or cancel the notification

--- NotifyMe/app/src/main/java/com/m6code/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/m6code/notifyme/MainActivity.java
@@ -8,6 +8,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
@@ -16,6 +18,8 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button_notify;
+    private Button button_cancel;
+    private Button button_update;
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private NotificationManager mNotifyManager;
     private static final int NOTIFICATION_ID = 0;
@@ -30,12 +34,55 @@ public class MainActivity extends AppCompatActivity {
             sendNotification();
         });
 
+        button_update = findViewById(R.id.update);
+        button_update.setOnClickListener(view -> {
+            // Update the notification
+            updateNotification();
+        });
+
+        button_cancel = findViewById(R.id.cancel);
+        button_cancel.setOnClickListener(view -> {
+            // Cancel the notification
+            cancelNotification();
+        });
+
+        setNotificationButtonState(true, false, false);
+
         createNotificationChannel();
+    }
+
+    void setNotificationButtonState(Boolean isNotifyEnabled,
+                                    Boolean isUpdateEnabled,
+                                    Boolean isCancelEnabled){
+        button_notify.setEnabled(isNotifyEnabled);
+        button_update.setEnabled(isUpdateEnabled);
+        button_cancel.setEnabled(isCancelEnabled);
     }
 
     public void sendNotification() {
         NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
         mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+        setNotificationButtonState(false, true, true);
+    }
+
+    public void updateNotification() {
+        Bitmap androidImage = BitmapFactory
+                .decodeResource(getResources(), R.drawable.mascot_1);
+
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(androidImage)
+                .setBigContentTitle("Notification Updated!"));
+
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+
+        setNotificationButtonState(false, false, true);
+
+    }
+
+    public void cancelNotification() {
+        mNotifyManager.cancel(NOTIFICATION_ID);
+        setNotificationButtonState(true, false, false);
     }
 
     public void createNotificationChannel() {
@@ -54,7 +101,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public NotificationCompat.Builder getNotificationBuilder(){
+    public NotificationCompat.Builder getNotificationBuilder() {
+
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent notifcationPendingIntent = PendingIntent.getActivity(this,
                 NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
