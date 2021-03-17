737235e54d71255462fe5aecd64f228b2f2430d6
M6code <benjamynfrank101@gmail.com>
2021-03-17 11:37:47 +0100

    The Alarm Manager : Task 2 - Set up the notification

--- StandUp/app/src/main/java/com/m6code/standup/MainActivity.java
+++ StandUp/app/src/main/java/com/m6code/standup/MainActivity.java
@@ -1,6 +1,15 @@
 package com.m6code.standup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Toast;
@@ -8,24 +17,75 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mNotificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
         ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
         alarmToggle.setOnCheckedChangeListener((buttonView, isChecked) -> {
             String toastMsg;
-            if (isChecked){
+            if (isChecked) {
                 // Set the toast message for the "on" case.
+                deliverNotification(MainActivity.this);
                 toastMsg = getString(R.string.stand_up_on);
-            }else {
+            } else {
                 // Set the toast message for the "off" case.
+                mNotificationManager.cancelAll();
                 toastMsg = getString(R.string.stand_up_off);
             }
 
             // Show a toast to say the alarm is turned on or off.
             Toast.makeText(MainActivity.this, toastMsg, Toast.LENGTH_SHORT).show();
         });
+
+        createNotificationChannel();
+    }
+
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
+    public void createNotificationChannel() {
+        // Create a notification manager object.
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    PRIMARY_CHANNEL_ID,
+                    getString(R.string.stand_up_notification),
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setDescription(
+                    getString(R.string.notifies_every_15_minutes_to_stand_up_and_walk));
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context) {
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(context,
+                NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_stand_up)
+                        .setContentTitle(getString(R.string.stand_up_alert))
+                        .setContentText(getString(R.string.you_should_stand_up))
+                        .setContentIntent(contentPendingIntent)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setAutoCancel(true)
+                        .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
